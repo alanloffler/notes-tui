68f72864eacd119ef2f16fca4f224779f9fe299d
alanloffler <alanmatiasloffler@gmail.com>
2026-05-15 21:48:26 -0300

    feat(app): textarea styles

--- internal/app/model.go
+++ internal/app/model.go
@@ -7,6 +7,7 @@ import (
 	"charm.land/bubbles/v2/textinput"
 	tea "charm.land/bubbletea/v2"
 	"github.com/alanloffler/notes-tui/internal/note"
+	"github.com/alanloffler/notes-tui/internal/styles"
 )
 
 const (
@@ -32,13 +33,33 @@ func New(store note.Store) Model {
 		log.Fatalf("unable to get notes: %v", err)
 	}
 
-	return Model{
+	model := Model{
 		state:     listView,
 		store:     store,
 		notes:     notes,
 		textarea:  textarea.New(),
 		textinput: textinput.New(),
 	}
+
+	model.textarea.SetWidth(50)
+	model.textarea.Placeholder = "Write the note content"
+
+	// tmp fix to set color on border
+	model.textarea.SetPromptFunc(2, func(_ textarea.PromptInfo) string {
+		return "│ "
+	})
+
+	st := model.textarea.Styles()
+	st.Focused.Prompt = styles.PromptStyle.Faint(true)
+	st.Focused.LineNumber = styles.LineNumberStyle.Faint(true)
+	st.Focused.CursorLineNumber = styles.CursorLineNumberStyle
+	st.Blurred.Prompt = styles.PromptStyle.Faint(true)
+	st.Blurred.LineNumber = styles.LineNumberStyle.Faint(true)
+	st.Blurred.CursorLineNumber = styles.CursorLineNumberStyle
+
+	model.textarea.SetStyles(st)
+
+	return model
 }
 
 func (m Model) Init() tea.Cmd {
